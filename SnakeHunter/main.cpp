554f695539db5cfc362ba2507f5e2d3efe900ef0
danhhao2005 <23021544@vnu.edu.vn>
2024-04-07 21:58:36 +0700

    .

--- SnakeHunter/main.cpp
+++ SnakeHunter/main.cpp
@@ -1,214 +1,202 @@
 #include <iostream>
-#include "mylib.h"
-#define MAX 100
+#include <SDL.h>
+#include <SDL_timer.h>
+#include <SDL_image.h>
+#include <vector>
+#include <ctime>
+
+#ifdef _WIN32
+#undef main
+#endif
+
 using namespace std;
-int toa_dox[MAX] = { 0 };
-int toa_doy[MAX] = { 0 };
-int sl = 4;
-int xqua = -1;
-int yqua = -1;
 
-void ve_tuong();
-void khoi_tao_ran();
-void play();
-void ve_ran();
-void di_chuyen_ran(int x, int y);
-bool game_over();
-void tao_qua();
-void ve_qua();
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int CELL_SIZE = 20;
+const int GRID_WIDTH = SCREEN_WIDTH / CELL_SIZE;
+const int GRID_HEIGHT = SCREEN_HEIGHT / CELL_SIZE;
 
-bool kt_ran_an_qua();
-void xu_ly_ran_an_qua();
-bool kt_ran_cham_than();
+struct Point {
+    int x, y;
+    Point() : x(0), y(0) {}
+    Point(int _x, int _y) : x(_x), y(_y) {}
+};
 
-int main()
-{
-	srand(time(NULL));
-	play();
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
 
-	_getch();
-	return 0;
+class SnakeGame {
+public:
+    SnakeGame();
+    ~SnakeGame();
+
+    void run();
+
+private:
+    void handleInput();
+    void update();
+    void render();
+    void placeFood();
+    bool checkCollision(Point p);
+    void gameOver();
+
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    vector<Point> snake;
+    Point food;
+    Direction dir;
+    bool isRunning;
+};
+
+SnakeGame::SnakeGame() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    snake.push_back(Point(GRID_WIDTH / 2, GRID_HEIGHT / 2));
+    placeFood();
+
+    dir = RIGHT;
+    isRunning = true;
 }
 
-void ve_tuong()
-{
-	for (int x = 10;x <= 95;x++)
-	{
-		gotoXY(x, 1);
-		cout << "+";
-		gotoXY(x, 25);
-		cout << "+";
-	}
-	for (int y = 1;y <= 25;y++)
-	{
-		gotoXY(10, y);
-		cout << "+";
-		gotoXY(95, y);
-		cout << "+";
-	}
+SnakeGame::~SnakeGame() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
-void play()
-{
-	ShowCur(0);
-	SetColor(11);
 
-	ve_tuong();
-	khoi_tao_ran();
-	tao_qua();
-	ve_qua();
-	int x = toa_dox[0];
-	int y = toa_doy[0];
-	int check = 2;//0: đi xuống
-				//1: đi lên
-				//2: đi qua phải
-				//3: đi qua trái
+void SnakeGame::run() {
+    while (isRunning) {
+        handleInput();
+        update();
+        render();
+        SDL_Delay(100);
+    }
+}
 
-	while (true)
-	{
-		gotoXY(toa_dox[sl], toa_doy[sl]);
-		cout << " ";
+void SnakeGame::handleInput() {
+    SDL_Event event;
+    if (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            isRunning = false;
+        }
+        else if (event.type == SDL_KEYDOWN) {
+            switch (event.key.keysym.sym) {
+                case SDLK_UP:
+                    if (dir != DOWN)
+                        dir = UP;
+                    break;
+                case SDLK_DOWN:
+                    if (dir != UP)
+                        dir = DOWN;
+                    break;
+                case SDLK_LEFT:
+                    if (dir != RIGHT)
+                        dir = LEFT;
+                    break;
+                case SDLK_RIGHT:
+                    if (dir != LEFT)
+                        dir = RIGHT;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
 
-		ve_ran();
+void SnakeGame::update() {
+    // Move the snake
+    Point next = snake.front();
+    switch (dir) {
+        case UP:
+            next.y -= 1;
+            break;
+        case DOWN:
+            next.y += 1;
+            break;
+        case LEFT:
+            next.x -= 1;
+            break;
+        case RIGHT:
+            next.x += 1;
+            break;
+    }
 
-		if (_kbhit())
-		{
-			char c = _getch();
-			if (c == -32)
-			{
-				c = _getch();
-				if (c == 72 && check != 0)
-				{
-					check = 1;
-				}
-				else if (c == 80 && check != 1)
-				{
-					check = 0;
-				}
-				else if (c == 75 && check != 2)
-				{
-					check = 3;
-				}
-				else if (c == 77 && check != 3)
-				{
-					check = 2;
-				}
-			}
-		}
+    // Check if snake collides with itself or wall
+    if (next.x < 0 || next.x >= GRID_WIDTH || next.y < 0 || next.y >= GRID_HEIGHT || checkCollision(next)) {
+        gameOver();
+        return;
+    }
 
-		if (check == 0)
-		{
-			y++;
-		}
-		else if (check == 1)
-		{
-			y--;
-		}
-		else if (check == 2)
-		{
-			x++;
-		}
-		else if (check ==3)
-		{
-			x--;
-		}
+    snake.insert(snake.begin(), next);
 
-		if (game_over())
-		{
-			break;
-		}
-		xu_ly_ran_an_qua();
+    // Check if snake eats food
+    if (next.x == food.x && next.y == food.y) {
+        placeFood();
+    } else {
+        snake.pop_back();
+    }
+}
 
-		di_chuyen_ran(x,y);
+void SnakeGame::render() {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
-		Sleep(100);
-	}
+    // Render snake
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    for (Point const &p : snake) {
+        SDL_Rect rect = { p.x * CELL_SIZE, p.y * CELL_SIZE, CELL_SIZE, CELL_SIZE };
+        SDL_RenderFillRect(renderer, &rect);
+    }
+
+    // Render food
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_Rect rect = { food.x * CELL_SIZE, food.y * CELL_SIZE, CELL_SIZE, CELL_SIZE };
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
 }
-void khoi_tao_ran()
-{
-	int x_khoi_tao = 50;
-	int y_khoi_tao = 13;
-	for (int i = 0;i < sl;i++)
-	{
-		toa_dox[i] = x_khoi_tao--;
-		toa_doy[i] = y_khoi_tao;
-	}
+
+void SnakeGame::placeFood() {
+    srand(time(NULL));
+    bool valid = false;
+    while (!valid) {
+        food.x = rand() % GRID_WIDTH;
+        food.y = rand() % GRID_HEIGHT;
+        valid = true;
+        for (Point const &p : snake) {
+            if (p.x == food.x && p.y == food.y) {
+                valid = false;
+                break;
+            }
+        }
+    }
 }
-void ve_ran()
-{
-	for (int i = 0;i < sl;i++)
-	{
-		gotoXY(toa_dox[i], toa_doy[i]);
-		if (i == 0)
-		{
-			cout << "0";
-		}
-		else
-		{
-			cout << "o";
-		}
-	}
+
+bool SnakeGame::checkCollision(Point p) {
+    for (size_t i = 1; i < snake.size(); ++i) {
+        if (snake[i].x == p.x && snake[i].y == p.y) {
+            return true;
+        }
+    }
+    return false;
 }
-void di_chuyen_ran(int x,int y)
-{
-	for (int i = sl;i > 0;i--)
-	{
-		toa_dox[i] = toa_dox[i-1];
-		toa_doy[i] = toa_doy[i-1];
-	}
-	toa_dox[0] = x;
-	toa_doy[0] = y;
+
+void SnakeGame::gameOver() {
+    cout << "Game Over!" << endl;
+    isRunning = false;
 }
-bool kt_ran_cham_than()
-{
-	for (int i = 1;i <= sl;i++)
-	{
-		if (toa_dox[0] == toa_dox[i] && toa_doy[0] == toa_doy[i])
-		{
-			return true;
-		}
-	}
-	return false;
-}
-bool game_over()
-{
-	if (toa_dox[0] == 10 || toa_dox[0] == 95 || toa_doy[0] == 1 || toa_doy[0] == 25)
-	{
-		return true;
-	}
-	return kt_ran_cham_than();
-}
-bool kt_ran_de_qua()
-{
-	for (int i = 0;i < sl;i++)
-	{
-		if (xqua == toa_dox[i] && yqua == toa_doy[i]) {
-			return true;
-		}
-	}
-	return false;
-}
-void tao_qua()
-{
-	do
-	{
-		xqua = rand() % (94 - 11 + 1) + 11;
-		yqua = rand() % (25 - 1 + 1) + 1;
-	}while(kt_ran_de_qua());
-}
-void ve_qua()
-{
-	gotoXY(xqua, yqua);cout << "$";
-}
-bool kt_ran_an_qua()
-{
-	return (toa_dox[0] == xqua && toa_doy[0] == yqua);
-}
-void xu_ly_ran_an_qua()
-{
-	if (kt_ran_an_qua())
-	{
-		sl++;
-		tao_qua();
-		ve_qua();
-	}
+
+int main() {
+    SnakeGame game;
+    game.run();
+    return 0;
 }
