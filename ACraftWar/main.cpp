c83df7e049e0e9c9362dcf3f67a8992baf21b30c
danhhao2005 <23021544@vnu.edu.vn>
2024-04-30 10:50:41 +0700

    .

---
+++ ACraftWar/main.cpp
@@ -0,0 +1,20 @@
+
+#include "GameMain.h"
+
+using namespace std;
+
+#undef main
+int main()
+{
+    bool bInit = GameMain::GetInstance()->Init();
+    bool bLoadData = GameMain::GetInstance()->LoadData();
+    if (bInit == true && bLoadData == true)
+    {
+        // Call Loop Function here
+        GameMain::GetInstance()->Loop();
+    }
+
+    GameMain::GetInstance()->Close();
+    GameMain::GetInstance()->FreeInstance();
+    return 0;
+}
